d921d21712268945a6da189ed886f645ef7bf2d8
Transporter 1 <lynx.thoi@emagegroup.com>
2025-09-09 17:15:52 +0800

    update config file

---
+++ work_dir/fcos3d_regnet/fcos3d_regnet_fpn_head-gn_3x_adam-mono3d.py
@@ -0,0 +1,350 @@
+albu_train_transforms = [
+    dict(p=0.5, type='RandomBrightnessContrast'),
+    dict(p=0.2, type='MotionBlur'),
+    dict(p=0.2, type='GaussNoise'),
+]
+auto_scale_lr = dict(base_batch_size=32, enable=False)
+backend_args = None
+class_names = [
+    'guilder',
+]
+data_root = '/home/alan_khang/Desktop/adam_dataset'
+dataset_type = 'NuScenesDataset'
+default_hooks = dict(
+    checkpoint=dict(
+        interval=1,
+        max_keep_ckpts=1,
+        rule='greater',
+        save_best=
+        'NuScenes metric/pred_instances_3d_NuScenes/guilder_AP_dist_0.5',
+        type='CheckpointHook'),
+    logger=dict(interval=50, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    timer=dict(type='IterTimerHook'),
+    visualization=dict(type='Det3DVisualizationHook'))
+default_scope = 'mmdet3d'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+input_modality = dict(use_camera=True, use_lidar=False)
+launcher = 'none'
+load_from = None
+log_level = 'INFO'
+log_processor = dict(by_epoch=True, type='LogProcessor', window_size=50)
+lr = 0.008
+metainfo = dict(classes=[
+    'guilder',
+])
+model = dict(
+    backbone=dict(
+        arch='regnetx_4.0gf',
+        frozen_stages=1,
+        init_cfg=dict(
+            checkpoint='open-mmlab://regnetx_4.0gf', type='Pretrained'),
+        norm_cfg=dict(requires_grad=True, type='BN'),
+        norm_eval=True,
+        out_indices=(
+            0,
+            1,
+            2,
+            3,
+        ),
+        style='pytorch',
+        type='mmdet.RegNet'),
+    bbox_head=dict(
+        attr_branch=(256, ),
+        bbox_coder=dict(code_size=9, type='FCOS3DBBoxCoder'),
+        center_sampling=True,
+        centerness_on_reg=True,
+        cls_branch=(256, ),
+        conv_bias=True,
+        dcn_on_last_conv=True,
+        diff_rad_by_sin=True,
+        dir_branch=(256, ),
+        dir_limit_offset=0,
+        dir_offset=0.7854,
+        feat_channels=256,
+        group_reg_dims=(
+            2,
+            1,
+            3,
+            1,
+            2,
+        ),
+        in_channels=256,
+        loss_attr=dict(
+            loss_weight=1.0, type='mmdet.CrossEntropyLoss', use_sigmoid=False),
+        loss_bbox=dict(
+            beta=0.1111111111111111,
+            loss_weight=1.0,
+            type='mmdet.SmoothL1Loss'),
+        loss_centerness=dict(
+            loss_weight=1.0, type='mmdet.CrossEntropyLoss', use_sigmoid=True),
+        loss_cls=dict(
+            alpha=0.25,
+            gamma=2.0,
+            loss_weight=1.0,
+            type='mmdet.FocalLoss',
+            use_sigmoid=True),
+        loss_dir=dict(
+            loss_weight=1.0, type='mmdet.CrossEntropyLoss', use_sigmoid=False),
+        norm_on_bbox=True,
+        num_classes=1,
+        pred_attrs=True,
+        pred_velo=True,
+        reg_branch=(
+            (256, ),
+            (256, ),
+            (256, ),
+            (256, ),
+            (),
+        ),
+        stacked_convs=2,
+        strides=[
+            8,
+            16,
+            32,
+            64,
+            128,
+        ],
+        type='FCOSMono3DHead',
+        use_direction_classifier=True),
+    data_preprocessor=dict(
+        bgr_to_rgb=False,
+        mean=[
+            103.53,
+            116.28,
+            123.675,
+        ],
+        pad_size_divisor=32,
+        std=[
+            1.0,
+            1.0,
+            1.0,
+        ],
+        type='Det3DDataPreprocessor'),
+    neck=dict(
+        add_extra_convs='on_output',
+        in_channels=[
+            80,
+            240,
+            560,
+            1360,
+        ],
+        num_outs=5,
+        out_channels=256,
+        relu_before_extra_convs=True,
+        start_level=1,
+        type='mmdet.FPN'),
+    test_cfg=dict(
+        max_per_img=200,
+        min_bbox_size=0,
+        nms_across_levels=False,
+        nms_pre=1000,
+        nms_thr=0.8,
+        score_thr=0.05,
+        use_rotate_nms=True),
+    train_cfg=dict(
+        allowed_border=0,
+        code_weight=[
+            1.0,
+            1.0,
+            0.2,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            0.05,
+            0.05,
+        ],
+        debug=False,
+        pos_weight=-1),
+    type='FCOSMono3D')
+optim_wrapper = dict(
+    clip_grad=dict(max_norm=35, norm_type=2),
+    optimizer=dict(lr=0.0001, type='AdamW', weight_decay=0.01),
+    type='OptimWrapper')
+param_scheduler = [
+    dict(
+        begin=0,
+        by_epoch=False,
+        end=500,
+        start_factor=0.3333333333333333,
+        type='LinearLR'),
+    dict(
+        begin=0,
+        by_epoch=True,
+        end=12,
+        gamma=0.1,
+        milestones=[
+            8,
+            11,
+        ],
+        type='MultiStepLR'),
+]
+resume = False
+test_cfg = dict(type='TestLoop')
+test_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        ann_file='nuscenes_format/guilder_infos_val.pkl',
+        backend_args=None,
+        box_type_3d='Camera',
+        data_prefix=dict(CAM_FRONT='', pts=''),
+        data_root='/home/alan_khang/Desktop/adam_dataset',
+        load_type='mv_image_based',
+        metainfo=dict(classes=[
+            'guilder',
+        ]),
+        modality=dict(use_camera=True, use_lidar=False),
+        pipeline=[
+            dict(backend_args=None, type='LoadImageFromFileMono3D'),
+            dict(scale_factor=1.0, type='mmdet.Resize'),
+            dict(keys=[
+                'img',
+            ], type='Pack3DDetInputs'),
+        ],
+        test_mode=True,
+        type='NuScenesDataset',
+        use_valid_flag=True),
+    drop_last=False,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = dict(
+    ann_file=
+    '/home/alan_khang/Desktop/adam_dataset/nuscenes_format/guilder_infos_val.pkl',
+    backend_args=None,
+    data_root='/home/alan_khang/Desktop/adam_dataset',
+    metric='bbox',
+    type='NuScenesMetric')
+test_pipeline = [
+    dict(backend_args=None, type='LoadImageFromFileMono3D'),
+    dict(scale_factor=1.0, type='mmdet.Resize'),
+    dict(keys=[
+        'img',
+    ], type='Pack3DDetInputs'),
+]
+train_cfg = dict(max_epochs=36, type='EpochBasedTrainLoop', val_interval=1)
+train_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        ann_file='nuscenes_format/guilder_infos_train.pkl',
+        backend_args=None,
+        box_type_3d='Camera',
+        data_prefix=dict(CAM_FRONT='', pts=''),
+        data_root='/home/alan_khang/Desktop/adam_dataset',
+        load_type='mv_image_based',
+        metainfo=dict(classes=[
+            'guilder',
+        ]),
+        modality=dict(use_camera=True, use_lidar=False),
+        pipeline=[
+            dict(backend_args=None, type='LoadImageFromFileMono3D'),
+            dict(
+                type='LoadAnnotations3D',
+                with_attr_label=True,
+                with_bbox=True,
+                with_bbox_3d=True,
+                with_bbox_depth=True,
+                with_label=True,
+                with_label_3d=True),
+            dict(keep_ratio=True, scale=(
+                640,
+                400,
+            ), type='mmdet.Resize'),
+            dict(flip_ratio_bev_horizontal=0.0, type='RandomFlip3D'),
+            dict(
+                keys=[
+                    'img',
+                    'gt_bboxes',
+                    'gt_bboxes_labels',
+                    'attr_labels',
+                    'gt_bboxes_3d',
+                    'gt_labels_3d',
+                    'centers_2d',
+                    'depths',
+                ],
+                type='Pack3DDetInputs'),
+        ],
+        test_mode=False,
+        type='NuScenesDataset',
+        use_valid_flag=True),
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(shuffle=True, type='DefaultSampler'))
+train_pipeline = [
+    dict(backend_args=None, type='LoadImageFromFileMono3D'),
+    dict(
+        type='LoadAnnotations3D',
+        with_attr_label=True,
+        with_bbox=True,
+        with_bbox_3d=True,
+        with_bbox_depth=True,
+        with_label=True,
+        with_label_3d=True),
+    dict(keep_ratio=True, scale=(
+        640,
+        400,
+    ), type='mmdet.Resize'),
+    dict(flip_ratio_bev_horizontal=0.0, type='RandomFlip3D'),
+    dict(
+        keys=[
+            'img',
+            'gt_bboxes',
+            'gt_bboxes_labels',
+            'attr_labels',
+            'gt_bboxes_3d',
+            'gt_labels_3d',
+            'centers_2d',
+            'depths',
+        ],
+        type='Pack3DDetInputs'),
+]
+val_cfg = dict(type='ValLoop')
+val_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        ann_file='nuscenes_format/guilder_infos_val.pkl',
+        backend_args=None,
+        box_type_3d='Camera',
+        data_prefix=dict(CAM_FRONT='', pts=''),
+        data_root='/home/alan_khang/Desktop/adam_dataset',
+        load_type='mv_image_based',
+        metainfo=dict(classes=[
+            'guilder',
+        ]),
+        modality=dict(use_camera=True, use_lidar=False),
+        pipeline=[
+            dict(backend_args=None, type='LoadImageFromFileMono3D'),
+            dict(scale_factor=1.0, type='mmdet.Resize'),
+            dict(keys=[
+                'img',
+            ], type='Pack3DDetInputs'),
+        ],
+        test_mode=True,
+        type='NuScenesDataset',
+        use_valid_flag=True),
+    drop_last=False,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = dict(
+    ann_file=
+    '/home/alan_khang/Desktop/adam_dataset/nuscenes_format/guilder_infos_val.pkl',
+    backend_args=None,
+    data_root='/home/alan_khang/Desktop/adam_dataset',
+    metric='bbox',
+    type='NuScenesMetric')
+vis_backends = [
+    dict(type='LocalVisBackend'),
+]
+visualizer = dict(
+    name='visualizer',
+    type='Det3DLocalVisualizer',
+    vis_backends=[
+        dict(type='LocalVisBackend'),
+    ])
+work_dir = './work_dirs/fcos3d_regnet_adam'
